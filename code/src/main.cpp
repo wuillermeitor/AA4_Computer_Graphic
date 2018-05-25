f84aa3126a5ab46afb04017c1c4055a4aec1a142
Guillermo Sánchez <guillermosanchezsanchez@enti.cat>
2018-05-25 11:02:01 +0200

    y funciona el cargar modelos bien

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** argv) {
 	SDL_GLContext maincontext;
 
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -141,7 +141,7 @@ int main(int argc, char** argv) {
 
 
 		SDL_GL_SwapWindow(mainwindow);
-		waitforFrameEnd();
+		//waitforFrameEnd();
 	}
 
 	//myCleanupCode();
